8b537c459ff583ac17e22cda0a0489fbb447ccb2
TreeStain <Roguesgonnasneak@gmail.com>
2017-01-09 16:01:35 +1100

    Added the form and HTML

--- server.py
+++ server.py
@@ -1,8 +1,41 @@
 from tornado.ncss import Server
 
-def index(response):
-    response.write("Hello World")
+FORM = '''
+<!DOCTYPE html>
+<html>
+<head>
+generic text
+</head>
+<body>
+<form method="get">
+Enter your name <input type="text" name="name"/>
+<input type="submit"/>
+</form>
+</body>
+</html>
+'''
+
+RESULT = '''
+<!DOCTYPE html>
+<html>
+<head>
+generic text
+</head>
+<body>
+<h1>Hello {person}</h1>
+</body>
+</html>
+'''
+
+
+def test_get(response):
+    name = response.get_field("name")
+    if name is not None:
+        response.write(RESULT.format(person=name))
+    else:
+        response.write(FORM)
+
 
 server = Server()
-server.register("/",index)
+server.register("/", test_get)
 server.run()
