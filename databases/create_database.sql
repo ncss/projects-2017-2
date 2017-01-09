7ec559a46eda78d3fd72ad67121d12d5a8206ed1
ChiCubed <albert.chejin.edward.smith@outlook.com>
2017-01-09 16:11:17 +1100

    Make database create date by default

--- databases/create_database.sql
+++ databases/create_database.sql
@@ -1,14 +1,14 @@
 CREATE TABLE profiles (
 	id INT PRIMARY KEY,
-	username TEXT,
-	password TEXT,
+	username TEXT UNIQUE,
+	password TEXT, 
 	email TEXT
 );
 
 CREATE TABLE images (
 	id INT PRIMARY KEY,
 	user_id INT FOREIGN KEY REFERENCES profiles(id),
-	date TEXT
+	date DATETIME DEFAULT (DATETIME('NOW'))
 );
 
 CREATE TABLE comments (
@@ -17,11 +17,23 @@ CREATE TABLE comments (
 	reply_id INT FOREIGN KEY REFERENCES comments(id),
 	image_id INT FOREIGN KEY REFERENCES images(id),
 	contents TEXT,
-	date TEXT
+	date DATETIME DEFAULT (DATETIME('NOW'))
+);
+
+CREATE TABLE categories (
+	id INT PRIMARY KEY,
+	name TEXT -- category name
 );
 
 CREATE TABLE imagecategories (
 	id INT PRIMARY KEY,
 	category_id INT FOREIGN KEY REFERENCES categories(id),
 	image_id INT FOREIGN KEY REFERENCES images(id)
+);
+
+CREATE TABLE votes (
+	id INT PRIMARY KEY,
+	user_id INT FOREIGN KEY REFERENCES profiles(id),
+	comment_id INT FOREIGN KEY REFERENCES comments(id),
+	is_upvote INT -- SQLite does not technically have a bool
 );
