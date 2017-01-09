4f491b011bb7ae235d70030a459bc9cb52bd9c5c
tastelessmemes <jessicayjliu1@gmail.com>
2017-01-09 17:42:28 +1100

    mok data for SQL for comments finished

--- databases/mock_data.sql
+++ databases/mock_data.sql
@@ -28,3 +28,26 @@ comments = [
 (Id, Username, Password, Email)
 
 INSERT INTO users VALUES (      "989898", "2-Elite-4-u", "codislife", "THEPRO@mlg.com");
+
+
+
+
+
+
+--STATEMENT-DELIM
+(id, poster_id, reply_to, contents, dates, image_id)
+--STATEMENT-DELIM
+INSERT INTO comments VALUES ( "1", "989898", None, "I can do better.", "1/1/2017", None);
+--STATEMENT-DELIM
+INSERT INTO comments VALUES ( "2", "452246", "4", "Me like", "1/1/2017", None);
+--STATEMENT-DELIM
+INSERT INTO comments VALUES ( "3", "987654", None, "I believe your choice of colours is incorrect. Add more warmth so you can add more life to it.", "1/1/2017", None);
+--STATEMENT-DELIM
+INSERT INTO comments VALUES ("4", "092455", None, "Is that a James Curran I see?", "1/1/2017", None);
+--STATEMENT-DELIM
+INSERT INTO comments VALUES ("5", "123456", None,: "1000000000000000 upvotes for you good sir!", "1/1/2017", : None);
+--STATEMENT-DELIM
+INSERT INTO comments VALUES ("6", "747025", "3", "Yeah, I agree.", "2/1/2017",  None);
+--STATEMENT-DELIM
+INSERT INTO comments VALUES ("7", "020520", "5", "Plus mine :)", "2/1/2017", None);
+--STATEMENT-DELIM
