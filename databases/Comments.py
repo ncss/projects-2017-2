c11c8fe76df0e648237cae89e32a596af6468d77
vineyk24 <vineyk24@gmail.com>
2017-01-09 16:56:56 +1100

    Added profile class with method for adding profile based on id.

---
+++ databases/Comments.py
@@ -0,0 +1,8 @@
+class Comments(object):
+    def __init__(self,fk_comment_id,poster,reply_to,contents,date):
+        self.fkcommentid=fkcommentid
+        self.poster=poster
+        self.replyTo=replyTo
+        self.contents=contents
+        self.date=date
+    
---
+++ databases/Profiles.py
@@ -0,0 +1,31 @@
+class Profiles(object):
+    def __init__(self,pkid,user,email):
+        self._id=pkid
+        self.user=user
+        self.email=email
+
+    @classmethod
+    def from_id(cls, pkid):
+        """
+         
+        """
+        # SQL select statement to retrieve
+        # the username and email
+        
+        return cls(pkid,user,email)
+
+    @staticmethod
+    def login(user,password):
+        """
+        Takes username and password
+        Returns userid.
+        This can then be used with Profiles.from_id
+        to return a new Profile object.
+        """
+        # Hash teh password
+        # SQL select statement to retrieve
+        # the userid
+        # Return None (or raise Error, you choose)
+        # if there is no user matching
+        # username and hashed password.
+        return pkid
