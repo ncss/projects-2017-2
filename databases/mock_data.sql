9caafd194e92bfb0b378d509ea32c777ad2f9d3e
ChiCubed <albert.chejin.edward.smith@outlook.com>
2017-01-10 10:58:03 +1100

    Edit mock data format

--- databases/mock_data.sql
+++ databases/mock_data.sql
@@ -1,7 +1,5 @@
 
---STATEMENT-DELIM
-(Id, Username, Password, Email)
-
+--(id, username, password, email)
 INSERT INTO users VALUES ("989898", "2-Elite-4-u", "codislife", "THEPRO@mlg.com");
 --STATEMENT-DELIM
 INSERT INTO users VALUES ("452246", "lolmemes", "PianoTuner", "The_Spiciest@live.com");
@@ -16,23 +14,16 @@ INSERT INTO users VALUES ("747025", "ksgiwfw", "weift823r2", "f23rb82bf_@gmail.c
 --STATEMENT-DELIM
 INSERT INTO users VALUES ("020520", "Becky", "idIfg34rer", "BeckyWilda@coles.com";
 --STATEMENT-DELIM
-
-
-INSERT INTO users VALUES (      "989898", "2-Elite-4-u", "codislife", "THEPRO@mlg.com");
-
-
-
-
-
+INSERT INTO users VALUES ("989898", "2-Elite-4-u", "codislife", "THEPRO@mlg.com");
 
 --STATEMENT-DELIM
-(id, poster_id, reply_to, contents, dates, image_id)
+
+--(id, poster_id, reply_to, contents, dates, image_id)
+INSERT INTO comments VALUES ("1", "989898", None, "I can do better.", "1/1/2017", None);
 --STATEMENT-DELIM
-INSERT INTO comments VALUES ( "1", "989898", None, "I can do better.", "1/1/2017", None);
+INSERT INTO comments VALUES ("2", "452246", "4", "Me like", "1/1/2017", None);
 --STATEMENT-DELIM
-INSERT INTO comments VALUES ( "2", "452246", "4", "Me like", "1/1/2017", None);
---STATEMENT-DELIM
-INSERT INTO comments VALUES ( "3", "987654", None, "I believe your choice of colours is incorrect. Add more warmth so you can add more life to it.", "1/1/2017", None);
+INSERT INTO comments VALUES ("3", "987654", None, "I believe your choice of colours is incorrect. Add more warmth so you can add more life to it.", "1/1/2017", None);
 --STATEMENT-DELIM
 INSERT INTO comments VALUES ("4", "092455", None, "Is that a James Curran I see?", "1/1/2017", None);
 --STATEMENT-DELIM
@@ -41,4 +32,3 @@ INSERT INTO comments VALUES ("5", "123456", None,: "1000000000000000 upvotes for
 INSERT INTO comments VALUES ("6", "747025", "3", "Yeah, I agree.", "2/1/2017",  None);
 --STATEMENT-DELIM
 INSERT INTO comments VALUES ("7", "020520", "5", "Plus mine :)", "2/1/2017", None);
---STATEMENT-DELIM
