80a36d232e0da12f3e668802920e924c4995183b
vineyk24 <vineyk24@gmail.com>
2017-01-10 14:40:04 +1100

    We have a database ready!

--- databases/Profiles.py
+++ databases/Profiles.py
@@ -1,9 +1,28 @@
 import sqlite3
+import hashlib
+import random
+
 class Profiles(object):
-    def __init__(self,pkid,user,email):
+    def __init__(self,pkid,user,hashed_pass,email):
         self._id=pkid
         self.user=user
+        self.hashed_pass=hashed_pass
         self.email=email
+
+    def __repr__(self):
+        return "Profiles({},{},{},{})".format(self._id,self.user,self.hashed_pass,self.email)
+
+    def __str__(self):
+        return "Profile id {} with username: {}, hashed pass: {}, email: {}".format(self._id,self.user,self.hashed_pass,self.email)
+
+    @staticmethod
+    def _hash(password):
+        # Disable hashing
+        # return password
+
+        hash_object = hashlib.sha256(bytes(password))
+        hex_dig = hash_object.hexdigest()
+        return hex_dig;
 
     @classmethod
     def from_id(cls, pkid):
@@ -11,25 +30,48 @@ class Profiles(object):
         # the username and email
         conn=sqlite3.connect("data.db");
         cur=conn.cursor();
-        cur.execute("SELECT * FROM profiles WHERE id="+str(pkid));
+        cur.execute("SELECT * FROM profiles WHERE id=?;",(str(pkid),));
         row=cur.fetchone();
         print(row)
         conn.close()
-        pkid,user,email=(i for i in row)
-        return Profiles(pkid,user,email)
+        pkid,user,hashed_pass,email=row
+        return cls(pkid,user,hashed_pass,email)
 
 
     @classmethod
     def from_user(cls, username):
         conn=sqlite3.connect("data.db");
         cur=conn.cursor()
-        cur.execute("SELECT * FROM profiles WHERE user="+str(username));
+        cur.execute("SELECT * FROM profiles WHERE username=?;",(username,));
         row=cur.fetchone()
         conn.close()
-        pkid,user,email=(i for i in row)
-        return Profiles(pkid,user,email);
+        pkid,user,hashed_pass,email=row
+        return cls(pkid,user,hashed_pass,email)
 
-p=Profiles(1,"Viney","vineyk24@gmail.com");
+    @classmethod
+    def register(cls,username,password,email):
+        conn=sqlite3.connect("data.db");
+        cur=conn.cursor()
+        if cur.execute("SELECT * FROM profiles WHERE username=? LIMIT 1;", (username,)).fetchone():
+            raise ValueError("Registration error: Username {} is already taken.".format(username))
+        if cur.execute("SELECT * FROM profiles WHERE email=? LIMIT 1;", (email,)).fetchone():
+            raise ValueError("Registration error: Email {} is already used.".format(email))
 
-p.from_id(1);
+        pkid = random.getrandbits(32)
+        while cur.execute("SELECT * FROM profiles WHERE id=?", (pkid,)).fetchone():
+            pkid = random.getrandbits(32)
+        cur.execute("INSERT INTO profiles VALUES (?,?,?,?)", (pkid,username,cls._hash(password),email))
+        conn.commit()
+        conn.close()
 
+        return cls(pkid, username, cls._hash(password), email)
+
+    @classmethod
+    def login(cls,username,password):
+        conn=sqlite3.connect("data.db")
+        cur=conn.cursor()
+        userdata = cur.execute("SELECT id,username,email FROM profiles WHERE username=? AND password=? LIMIT 1;",(username,cls._hash(password))).fetchone()
+        if userdata:
+            return cls(userdata[0],userdata[1],cls._hash(password),userdata[2])
+        else:
+            raise ValueError("Username {} does not exist or password is incorrect.".format(username));
--- databases/generate_db.py
+++ databases/generate_db.py
@@ -1,4 +1,4 @@
-import sqlite3, os
+import sqlite3, os, Profiles
 
 # Ensure database does not exist already
 os.remove('data.db')
@@ -7,5 +7,11 @@ cursor=conn.cursor()
 
 cursor.executescript(open("create_database.sql").read())
 cursor.executescript(open("mock_data.sql").read())
+
+for r in cursor.execute("SELECT id,password FROM profiles;").fetchall():
+    conn.execute("UPDATE profiles SET password = ? WHERE id = ?;", (Profiles.Profiles._hash(r[1]), r[0]))
+
+
+conn.commit()
 
 conn.close()
