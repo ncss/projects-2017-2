cd29a60e1d0d648aef3eeaee9d524ca8720c504f
sden5874 <rainbow_life@live.com.au>
2017-01-10 17:11:08 +1100

    Creating 'Creating Comments' function

--- databases/Comment.py
+++ databases/Comment.py
@@ -1,8 +1,24 @@
 from databases.BasicInfo import BasicInfo
+from databases.Profiles import Profiles
+import databases.db
+import sqlite3
+
+sql = databases.db.db()
 
 class Comment(BasicInfo):
     def __init__(self, user, *args):
         super().__init__(user, *args)
         self.reply_to = args[2]
 
-print(Comment.get(2).reply_to)
+    @classmethod
+    def CreateComment(self, comment_id, user_id, reply_to, image_id, contents, date):
+        sql.execute('''
+                INSERT INTO comments VALUES (?, ?, ?, ?, ?, ?)
+                ''', (comment_id, user_id, reply_to, image_id, contents, date,))
+        comment = sql.fetchone()
+        p = Profiles.from_id(user_id)
+        return cls(p, *comment)
+
+stub = ['1', '123', '4', 'NULL', 'Hello', '12/1/14' ]
+
+print("hi".CreateComment(0).contents)
