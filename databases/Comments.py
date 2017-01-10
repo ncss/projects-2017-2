37af7cfe3abf763537e76c734686bd56dcca694e
ChiCubed <albert.chejin.edward.smith@outlook.com>
2017-01-10 11:38:32 +1100

    Edit for consistency

--- databases/Comments.py
+++ databases/Comments.py
@@ -1,6 +1,6 @@
 class Comments(object):
     def __init__(self, pkid, poster_id, reply_to, image_id, contents, date=None):
-        self.pkid = pkid
+        self._id = pkid
         self.user_id = user_id
         self.reply_to = reply_to
         self.image_id = image_id
