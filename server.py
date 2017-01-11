a36957980f7f366fa8707ecfe72bade234419ca2
Peter(Yong) Zhong <peter@topnoch.net>
2017-01-12 01:29:29 +1100

    fix login dictyionary key for temp engine

--- server.py
+++ server.py
@@ -105,7 +105,7 @@ def see_photo_and_response(response):
 
 def image_get_upload(response):
     username = response.get_secure_cookie('username')
-    template = render_file('templates/uploadphotos.html', {'message': '', 'loged_in':username})
+    template = render_file('templates/uploadphotos.html', {'message': '', 'login':username})
     response.write(template)
 
 
@@ -116,7 +116,7 @@ def image_post_upload(response):
     content = response.get_field('content')
     username = response.get_secure_cookie('username')
     if not username:
-        template = render_file('templates/uploadphotos.html', {'message': 'You need to Log in first6'})
+        template = render_file('templates/uploadphotos.html', {'message': 'You need to Log in first', 'login':username})
         response.write(template)
     else:
         username = username.decode()
