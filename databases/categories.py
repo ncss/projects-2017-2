7d61dcf02647f3e70ef5cff1dfba5b54698f806b
sden5874 <rainbow_life@live.com.au>
2017-01-12 01:50:57 +1100

    Added Categories mock data

--- databases/categories.py
+++ databases/categories.py
@@ -1,17 +1,15 @@
 from databases.Profiles import Profiles
 
 class Categories():
-    def __init__(self, name, category_id, image_id):
-        self.name = name
+    def __init__(self, category_name, category_id, image_id):
+        self.category_name = category_name
         self.category_id = category_id
         self.image_id = image_id
 
     @classmethod
-    def get(cls, sql, id):
+    def get(cls, sql,):
         sql.execute('''
-            SELECT *
-            FROM comments WHERE id=?
-            ''', (id,))
-        id = sql.fetchone()
-        p = Profiles.from_id(id[1])
-        return cls(p, *id)
+            SELECT category_name
+            FROM categories
+            ''')
+        return cls(category_name)
